095f59ba87d4afd246180c8a7ac84fadcdba6ee6
jecaro <jeancharles.quillet@gmail.com>
2024-05-16 20:42:17 +0200

    Add daisyui
    `node2nix.nix` has been removed. It doesn't seem to yield to something
    buildable.
    
    The `node-*.nix` has been generated with the current version of
    nixpkgs#node2nix (stable).

--- static/src/node2nix.nix
+++
@@ -1,7 +0,0 @@
-(import
-  (builtins.fetchTarball {
-    name = "nixpkgs";
-    url = "https://github.com/NixOS/nixpkgs/archive/3aad50c30c826430b0270fcf8264c8c41b005403.tar.gz";
-    sha256 = "0xwqsf08sywd23x0xvw4c4ghq0l28w2ki22h0bdn766i16z9q2gr";
-  })
-  { }).nodePackages.node2nix
