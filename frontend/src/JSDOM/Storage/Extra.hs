49b8e10a022289de465d4482c31517a3824b9115
jecaro <jeancharles.quillet@gmail.com>
2024-10-03 10:10:47 +0200

    Catch js exceptions when loading the settings

--- frontend/src/JSDOM/Storage/Extra.hs
+++ frontend/src/JSDOM/Storage/Extra.hs
@@ -1,13 +1,25 @@
+{-# LANGUAGE QuasiQuotes #-}
+
 module JSDOM.Storage.Extra (save, load, clear) where
 
-import Control.Lens ((^.))
-import Control.Monad ((<=<))
+import Control.Monad.Trans.Maybe (MaybeT (..), runMaybeT)
+import Data.String.Interpolate (iii)
 import Data.Text (Text)
 import JSDOM (currentWindowUnchecked)
 import JSDOM.Storage (getItem, removeItem, setItem)
 import JSDOM.Types (Storage, ToJSVal (..))
 import JSDOM.Window (getLocalStorage)
-import Language.Javascript.JSaddle (FromJSVal (..), JSM, JSString, JSVal, js1, jsg, valToJSON)
+import Language.Javascript.JSaddle
+  ( FromJSVal (..),
+    JSM,
+    JSString,
+    JSVal,
+    call,
+    eval,
+    global,
+    valIsNull,
+    valToJSON,
+  )
 
 getLocalStorageUnchecked :: JSM Storage
 getLocalStorageUnchecked = currentWindowUnchecked >>= getLocalStorage
@@ -17,15 +29,35 @@ save key val = do
   ls <- getLocalStorageUnchecked
   setItem ls key =<< valToJSON val
 
+-- Parse a JSON string, returns null on any error
+safeParseJSON :: JSString -> JSM JSVal
+safeParseJSON json = call (eval script) global [json]
+  where
+    script :: Text
+    script =
+      [iii|
+      (function (str) {
+        try {
+          return JSON.parse(str);
+        }
+        catch (e) {
+          return null;
+        }
+      }
+      )|]
+
 load :: FromJSVal a => Text -> JSM (Maybe a)
 load key =
-  orNothing (fromJSVal <=< jsonParse)
-    =<< flip getItem key
-    =<< getLocalStorageUnchecked
+  runMaybeT $ do
+    jsString <- MaybeT $ flip getItem key =<< getLocalStorageUnchecked
+    jsVal <- MaybeT $ toMaybe =<< safeParseJSON jsString
+    MaybeT $ fromJSVal jsVal
   where
-    jsonParse :: JSString -> JSM JSVal
-    jsonParse string = jsg ("JSON" :: Text) ^. js1 ("parse" :: Text) string
-    orNothing = maybe (pure Nothing)
+    toMaybe :: JSVal -> JSM (Maybe JSVal)
+    toMaybe jsVal = do
+      valIsNull jsVal >>= \case
+        True -> pure Nothing
+        False -> pure $ Just jsVal
 
 clear :: Text -> JSM ()
 clear key = getLocalStorageUnchecked >>= flip removeItem key
